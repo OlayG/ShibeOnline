167d4fb7a6f7707e22386a75dd2393d361e2a43c
nicholassmock <cheesbat@aol.com>
2021-11-01 19:31:26 -0400

    puts the links on screen on button press

--- app/src/main/java/com/olayg/shibeonline/view/MainActivity.kt
+++ app/src/main/java/com/olayg/shibeonline/view/MainActivity.kt
@@ -3,24 +3,37 @@ package com.olayg.shibeonline.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.viewModels
 import com.olayg.shibeonline.R
+import com.olayg.shibeonline.databinding.ActivityMainBinding
 import com.olayg.shibeonline.viewmodel.ShibeViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private val viewModel by viewModels<ShibeViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Use this method to get the images
-        viewModel.getImages(10)
+        binding.shibeButton.setOnClickListener(this)
+//        viewModel.getImages(10)
 
         viewModel.shibes.observe(this) {
             // Here is where your will get the result
+            var urmom = binding.countText.text
             Log.d("MainActivity", "onCreate: $it")
+            var doges = it.toString()
+            binding.shibeOutput.text = doges
         }
+    }
+
+    override fun onClick(v: View?) {
+            when(v?.id){
+                R.id.shibeButton -> viewModel.getImages(10)
+            }
     }
 }
