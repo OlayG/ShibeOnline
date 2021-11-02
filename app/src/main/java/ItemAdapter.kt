f9be69ced3e85daea5fe007e3e22999441097d14
Jimmy McBride <jimmymcbride@jimmys-air.lan>
2021-11-02 19:29:48 -0400

    changes

--- app/src/main/java/ItemAdapter.kt
+++ app/src/main/java/ItemAdapter.kt
@@ -10,6 +10,7 @@ class ItemAdapter(private val context: Context, private val dataset: List<String
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.list_item)
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
 
