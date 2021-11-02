921d8961c6a3e81ad1bb82e606224d30f7e30bec
Cletis Jones <cletis.jones@mykenzie.snhu.edu>
2021-11-02 00:12:40 -0400

    hot fire

--- app/src/main/java/com/olayg/shibeonline/view/SecondActivity.kt
+++ app/src/main/java/com/olayg/shibeonline/view/SecondActivity.kt
@@ -1,9 +1,13 @@
 package com.olayg.shibeonline.view
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.olayg.shibeonline.R
 import com.olayg.shibeonline.databinding.ActivityScrollBinding
+
 
 class SecondActivity : AppCompatActivity() {
 
@@ -13,10 +17,25 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityScrollBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val dogs: ArrayList<String>? = intent.getStringArrayListExtra("images")
-        binding.scroller.text = dogs.toString()
 
-       // String names = getIntent().getStringExtra("Namehere");
+        val policy = ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+        val dogs: ArrayList<String>? = intent.getStringArrayListExtra("images")
+        if (dogs != null) {
+            for (index in dogs) {
+
+                val imgURL = index
+                val `in` = java.net.URL(imgURL).openStream()
+                var image = BitmapFactory.decodeStream(`in`)
+                val imageView = ImageView(this)
+                imageView.layoutParams = binding.scroller.layoutParams
+                imageView.setImageBitmap(image)
+                binding.scroller.addView(imageView)
+
+                // String names = getIntent().getStringExtra("Namehere");
+            }
+        }
     }
 
 
