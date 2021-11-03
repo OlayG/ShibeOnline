042f483d6f26c4479362dc06a5dc48e1bd63f363
Jimmy McBride <jimmymcbride@jimmys-air.lan>
2021-11-02 20:52:36 -0400

    finished homework

--- app/src/main/java/com/olayg/shibeonline/view/MainActivity.kt
+++ app/src/main/java/com/olayg/shibeonline/view/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.olayg.shibeonline.adapter.ShibeAdapter
 import com.olayg.shibeonline.databinding.ActivityMainBinding
 import com.olayg.shibeonline.viewmodel.ShibeViewModel
@@ -16,6 +19,8 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<ShibeViewModel>()
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val linear by lazy { LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false) }
+    private val grid by lazy { GridLayoutManager(this, 2) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,14 +33,24 @@ class MainActivity : AppCompatActivity() {
             Log.d("MainActivity", "onCreate: $it")
             (binding.rvImages.adapter as ShibeAdapter).updateUrls(it)
         }
+
+        binding.btnSwitch.setOnClickListener {
+            if (binding.rvImages.layoutManager == linear) {
+                binding.rvImages.layoutManager = grid
+            } else {
+                binding.rvImages.layoutManager = linear
+            }
+        }
     }
 
     private fun initViews() = with(binding) {
+
         btnFetch.setOnClickListener {
             val count = binding.etCount.text?.toString()?.toIntOrNull()
             // Use this method to get the images
             viewModel.getImages(count ?: 1)
         }
         rvImages.adapter = ShibeAdapter()
+        rvImages.layoutManager = linear
     }
 }
